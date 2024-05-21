945d6fb9cfb6d0e467897f475e49b90ff092a2a0
Vinicius Gomes <vinicius.svgomes@gmail.com>
2024-05-21 13:49:02 -0300

    Adiciona funcionalidade de favoritar

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -13,6 +13,10 @@ const createUserBody = z.object({
   password: z.string().min(6)
 })
 
+const likeGasStationParams = z.object({
+  gasStationId: z.coerce.number()
+})
+
 export class UserController {
   public async create(request: Request, response: Response) {
     const { name, email, password } = createUserBody.parse(request.body)
@@ -35,6 +39,112 @@ export class UserController {
       return response.status(201).json({ user: UserViewModel.toHTTP(user) })
     } else {
       throw new AppError(409, 'Email already registered.')
+    }
+  }
+
+  public async likesList(request: Request, response: Response) {
+    const { id: userId } = request.session
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId }
+    })
+
+    if (user) {
+      const likes = await prisma.like.findMany({
+        where: { userId },
+        select: {
+          gasStation: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              latitude: true,
+              longitude: true,
+              fuels: {
+                select: {
+                  id: true,
+                  name: true,
+                  price: true
+                }
+              }
+            }
+          }
+        }
+      })
+      const filteredLikes = likes.map(like => {
+        return {
+          ...like.gasStation
+        }
+      })
+      return response.status(200).json({ likes: filteredLikes })
+    } else {
+      throw new AppError(404, 'Authenticated user not found.')
+    }
+  }
+
+  public async likeGasStation(request: Request, response: Response) {
+    const { gasStationId } = likeGasStationParams.parse(request.params)
+    const { id: userId } = request.session
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId }
+    })
+
+    const gasStation = await prisma.gasStation.findUnique({
+      where: { id: gasStationId }
+    })
+
+    if (user) {
+      if (gasStation) {
+        const like = await prisma.like.findUnique({
+          where: { userId_gasStationId: { userId, gasStationId } }
+        })
+
+        if (!like) {
+          await prisma.like.create({ data: { userId, gasStationId } })
+          return response.status(204).send()
+        } else {
+          throw new AppError(409, 'Gas station already liked.')
+        }
+      } else {
+        throw new AppError(404, 'Gas station not found.')
+      }
+    } else {
+      throw new AppError(404, 'Authenticated user not found.')
+    }
+  }
+
+  public async unlikeGasStation(request: Request, response: Response) {
+    const { gasStationId } = likeGasStationParams.parse(request.params)
+    const { id: userId } = request.session
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId }
+    })
+
+    const gasStation = await prisma.gasStation.findUnique({
+      where: { id: gasStationId }
+    })
+
+    if (user) {
+      if (gasStation) {
+        const like = await prisma.like.findUnique({
+          where: { userId_gasStationId: { userId, gasStationId } }
+        })
+
+        if (like) {
+          await prisma.like.delete({
+            where: { userId_gasStationId: { userId, gasStationId } }
+          })
+          return response.status(204).send()
+        } else {
+          throw new AppError(409, 'Gas station not liked yet.')
+        }
+      } else {
+        throw new AppError(404, 'Gas station not found.')
+      }
+    } else {
+      throw new AppError(404, 'Authenticated user not found.')
     }
   }
 }
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express'
 
 import { UserController } from '@controllers/user-controller'
+import { ensureAuthenticated } from '@middlewares/ensure-authenticated'
 
 const userRoutes = Router()
 const userController = new UserController()
 
 userRoutes.post('/', userController.create)
+userRoutes.get('/likes', ensureAuthenticated, userController.likesList)
+userRoutes.post(
+  '/like/:gasStationId',
+  ensureAuthenticated,
+  userController.likeGasStation
+)
+userRoutes.post(
+  '/unlike/:gasStationId',
+  ensureAuthenticated,
+  userController.unlikeGasStation
+)
 
 export { userRoutes }
